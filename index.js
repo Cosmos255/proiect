8fd9460c56dbc8798248ae323bd74af64d42e232
Cosmos255 <150472944+Cosmos255@users.noreply.github.com>
2024-01-22 17:32:18 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+function page0(){
+    window.location.assign("https://cosmos255.github.io/proiect/index");
+}
+
+function page1(){
+    window.location.assign("https://cosmos255.github.io/proiect/page1");
+}
+
+function page2(){
+    window.location.assign("https://cosmos255.github.io/proiect/page2");
+}
+
+function page3(){
+    window.location.assign("https://cosmos255.github.io/proiect/page3");
+}
+
+function page4(){
+    window.location.assign("https://cosmos255.github.io/proiect/page4");
+}
+
+function page5(){
+    window.location.assign("https://cosmos255.github.io/proiect/page5");
+}
+
+function page6(){
+    window.location.assign("https://cosmos255.github.io/proiect/page6");
+}
+
+if (window.location.href == "https://cosmos255.github.io/proiect/index.html" ){
+    window.location.assign("https://cosmos255.github.io/proiect/index");
+}
+
+if (window.location.href == "https://cosmos255.github.io/proiect/index" ){
+    window.alert("Welcome to my project!")
+}
